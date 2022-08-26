653b8aea14d2debbdf726dea16dbf91c4d3165eb
AteetAgarwal <45282909+AteetAgarwal@users.noreply.github.com>
2022-08-26 11:05:02 +0530

    Update ChangePOwerAppOwner.ps1

--- ChangePOwerAppOwner.ps1
+++ ChangePOwerAppOwner.ps1
@@ -1,6 +1,6 @@
 Add-PowerAppsAccount
 $EnvironmentName = 'Default-c73a9ec7-32b7-4ada-9758-cf1149af345e'
-$AppName = (Get-AdminPowerApp | where {$_.DisplayName -eq 'FirstCanvasAppFromScratch' -and $_.EnvironmentName -eq 'Default-c73a9ec7-32b7-4ada-9758-cf1149af345e'} | Select AppName).AppName
+$AppName = (Get-AdminPowerApp | where {$_.DisplayName -eq 'FirstModelDrivenApp' -and $_.EnvironmentName -eq 'Default-c73a9ec7-32b7-4ada-9758-cf1149af345e'} | Select AppName).AppName
 $NewAppOwner = $Global:CurrentSession.userId
 
 Set-AdminPowerAppOwner -AppName $AppName -AppOwner $NewAppOwner -EnvironmentName $EnvironmentName
